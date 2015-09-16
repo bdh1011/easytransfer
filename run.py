d7ee5cb02d229939fea96b1a5d76961fe9d245d0
Ubuntu <ubuntu@ip-172-31-22-251.ap-southeast-1.compute.internal>
2015-09-16 20:34:26 +0000

    server

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import create_app
 if __name__ == "__main__":
     app = create_app()
     app.logger.info("Debug status is: " + str(app.config['DEBUG']))
-    app.run(debug=app.config.get('DEBUG'), host=app.config.get('HOST_IP'))
+    app.run(debug=app.config.get('DEBUG'), host='0.0.0.0', port=80)
